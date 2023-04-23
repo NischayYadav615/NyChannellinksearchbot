74e87ca6a1f56b8abc83d8792195b1ab9dc3ad60
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-04-23 09:42:46 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -2,12 +2,17 @@ class script(object):
     START = """**Hello {} 🤟**
    
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I Will filter your channel posts automatically and send it in your group chat when Someone search it.
+
+𝐒𝐞𝐧𝐝 𝐭𝐡𝐢𝐬 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 /help 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝚋𝚢 : @Nischaydv
+↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑"""
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request ☞ @Nischayydv.
 - After verification send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
@@ -17,18 +22,22 @@ this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Developed By @CyniteBackup
+    ABOUT = """Developed By @Nischayydv
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/Nischayydv'>Nischay</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
-✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://koyeb.com'>Koyeb</a>
 
-    STATS = """My Status 💫
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝚋𝚢 : @Nischaydv
+↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑"""
 
-👥 Users: {}
-🧿 Groups: {}"""
+    STATS = """𝙼𝚢 𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚘𝚛 𝚂𝚝𝚊𝚝𝚞𝚜 ⇅ ✓
+
+👥 ✓Users: {}
+🧿 ✓Groups: {}"""
 
     BROADCAST = """<u>{}</u>
 
